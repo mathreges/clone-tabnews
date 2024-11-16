34076a3ffe935f390c7ed706457f023e2f92ebaf
Matheus Reges <matheus.c.reges@gmail.com>
2024-11-16 11:23:05 -0300

    feat: Creating maintenance page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import { Maintenance } from './maintenance/index.js';
+
 export default Home;
 
 function Home() {
-  return <h1>Who is my little cat?</h1>;
+  let isMaintenanceEnable = true;
+
+  return isMaintenanceEnable ? <Maintenance /> : <h1>Who is my little cat?</h1>;
 }

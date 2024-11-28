ae132f659585e705357e2ec2491e82bce1281661
Matheus Reges <matheus.c.reges@gmail.com>
2024-11-28 07:09:00 -0300

    feat: Creating database client connection file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
+import database from "../../../../infra/database.js"
+
 export default status;
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;")
+
   response.status(200).json({ chave: "Alunos do curso.dev são pessoas acima da média" })
 }

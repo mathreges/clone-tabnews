91d0edab18a9c85dd040f0205bd060db21979b4d
Matheus Reges <matheus.c.reges@gmail.com>
2024-12-04 06:42:55 -0300

    fix: Fixing database connection logs on  and client connection throw erro

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client, Pool } from 'pg';
+import { Client } from 'pg';
 
 export default {
   query: query
@@ -12,6 +12,15 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD
   });
+
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  })
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -19,8 +28,12 @@ async function query(queryObject) {
   }
   catch (error) {
     console.error(error);
+    throw error;
   }
   finally {
+    if(!client._connected) {
+      return;
+    }
     await client.end();
   }
 }

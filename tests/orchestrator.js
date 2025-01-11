a559841c411bc951a7cbe41e09f9c07480e451c5
Matheus Reges <matheus.c.reges@gmail.com>
2025-01-11 14:44:37 -0300

    feat: Improving > curso.dev@1.0.0 test > npm run services:up && npm run wait-for-postgres && concurrently --names next,jest --hide next -k -s command-jest 'next dev' 'jest --runInBand --verbose'
    > curso.dev@1.0.0 services:up
    > docker compose --file infra/compose.yaml up -d
    
    > curso.dev@1.0.0 wait-for-postgres
    > node infra/scripts/wait-for-postgres.js
    
     🔴 Postgres is not ready.
    
     🟢 Postgres is ready and accepting connections.
    [jest] (node:34891) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
    [jest] (Use `node --trace-deprecation ...` to show where the warning was created)
    [jest] PASS tests/integration/api/v1/status/get.test.js
    [jest]   ✓ Get to  /api/v1/status/ should return 200 (29 ms)
    [jest]
    [jest] PASS tests/integration/api/v1/migrations/post.test.js
    [jest]   ✓ POST to  /api/v1/migrations/ should return 200 (62 ms)
    [jest]
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest]   ✓ Get to  /api/v1/migrations/ should return 200 (12 ms)
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       3 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        0.889 s, estimated 1 s
    [jest] Ran all test suites.
    [jest] jest --runInBand --verbose exited with code 0
    --> Sending SIGTERM to other processes.. perfomance with  and removing

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,16 +1,20 @@
 import retry from "async-retry";
 
 async function waitForAllServices() {
+  const statusEndpoint = "http://localhost:3000/api/v1/status";
   await waitForWebServices();
 
   async function waitForWebServices() {
     return retry(fetchStatusPage, {
-      retries: 120
+      retries: 120,
+      maxTimeout: 1000
     });
 
     async function fetchStatusPage() {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+      const response = await fetch(statusEndpoint);
+      if (!response.ok) {
+        throw Error();
+      }
     }
   }
 }

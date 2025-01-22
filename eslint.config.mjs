3117fd94a84e6c92cda08cac6277aa6aeb437dfa
Matheus Reges <matheus.c.reges@gmail.com>
2025-01-22 06:54:53 -0300

    fix: Addying configuration files for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,30 @@
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 import globals from "globals";
-import react from "eslint-plugin-react";
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["eslint:recommended", "prettier"],
+    extends: ["eslint:recommended", "prettier", "next"],
     rules: {
       semi: ["error"],
     },
     globals: { ...globals.browser, ...globals.node, ...globals.jest },
   }),
+  {
+    ignores: [
+      ".config/*",
+      "node_modules",
+      ".next",
+      ".swc",
+      ".vscode",
+      "infra/migrations/*",
+    ],
+  },
 ];
 
 export default eslintConfig;
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  eslint: {
+    dirs: ["pages", "infra/scripts", "infra/database.js", "models", "tests"],
+  },
+};
